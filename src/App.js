afaaa23d1baa4ae08bf08c0ab8a1c28e3a96b85f
sk2ller <kingofgame.85819@gmail.com>
2021-02-13 17:28:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -25,9 +25,3 @@ function App() {
 }
 
 export default App;
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/sk2ller/check.git
-git push -u origin main
